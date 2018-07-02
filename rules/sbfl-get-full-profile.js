73c0216f3c2eaa5e26975a915678b6a0be8b3bea
Tahmina Khatoon <tahmina@brainstation-23.com>
2018-07-02 13:54:02 +0600

    Add rules for get full profile

---
+++ rules/sbfl-get-full-profile.js
@@ -0,0 +1,23 @@
+function (user, context, callback) {
+    // console.log(user);
+    var data = {
+      locale: user.locale,
+      createdAt: user.created_at,
+      memberSince: user.memberSince,
+      weight: user.weight,
+      weightUnit: user.weightUnit,
+      offsetFromUTCMillis: user.offsetFromUTCMillis,
+      timezone: user.timezone
+    };
+    context.idToken['http://sbfl.com/profile/auth'] = user.identities[0];
+    context.idToken['http://sbfl.com/profile/identities'] = user.identities;
+    context.idToken['http://sbfl.com/profile/data'] = data;
+    context.idToken['http://sbfl.com/groups'] = user.groups;
+    // context.idToken['http://sbfl.com/createdAt'] = user.created_at;
+    // context.idToken['http://sbfl.com/memberSince'] = user.memberSince;
+    // context.idToken['http://sbfl.com/weight'] = user.weight;
+    // context.idToken['http://sbfl.com/weightUnit'] = user.weightUnit;
+    // context.idToken['http://sbfl.com/offsetFromUTCMillis'] = user.offsetFromUTCMillis;
+    // context.idToken['http://sbfl.com/timezone'] = user.timezone;
+    callback(null, user, context);    
+}
